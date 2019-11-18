7f890727c713788fe669abf143efe6086411bde6
shaaydar <shaaydar@yandex.ru>
2019-11-19 02:43:14 +0500

    init

---
+++ Siakod3-tree/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Siakod3_tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 508);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 539);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Идеально сбалансированное дерево";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Siakod3-tree/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Siakod3_tree
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var numbers = new int[26];
+            var random = new Random();
+            string inputString = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(10, 100);
+                inputString += numbers[i].ToString() + ", ";
+            }
+            
+
+            tree = Tree.MakeTree(numbers);
+
+            label1.Text = "Ввод: " + inputString;
+            label2.Text = "Вывод: " + Tree.Output(tree);
+        }
+
+        int[] test = { 55, 58, 59, 11, 18, 15, 33, 46, 98, 73, 21 };
+        Tree tree;
+        const int HEIGHT = 60;
+        const int RADIUS = 14;
+        const int TEXT_SIZE = 10;
+        Color COLOR_TEXT = Color.Black;
+        Color COLOR_NODE = Color.Red;
+
+
+        public void Draw(Graphics graphics, int width, int startX, Tree tree, int level = 0, bool isLeft = true)
+        {
+
+            if (tree.Left != null)
+            {
+                level++;
+                Draw(graphics, width / 2, startX, tree.Left, level, true);
+                level--;
+            }
+            if (tree.Right != null)
+            {
+                level++;
+                Draw(graphics, width / 2, startX + width / 2, tree.Right, level, false);
+                level--;
+            }
+
+            DrawNode(graphics, width, startX, tree.Data, level, isLeft);
+        }
+
+        public void DrawNode(Graphics graphics, int width, int startX, int data, int level, bool isLeft)
+        {
+            //центр верхнего узла
+            Point oldCenter = Point.Empty;
+            if (isLeft)
+            {
+                if (level == 0)
+                    oldCenter.Offset(startX + width / 2, (HEIGHT / 2));
+                else 
+                    oldCenter.Offset(startX + width, (HEIGHT / 2) + (HEIGHT * (level - 1)));
+            }
+            else
+            {
+                if (level == 0)
+                    oldCenter.Offset(startX, (HEIGHT / 2));
+                else 
+                    oldCenter.Offset(startX, (HEIGHT / 2) + (HEIGHT * (level - 1)));
+            }
+
+            //центр текущего узла
+            Point center = new Point(startX + width / 2, (HEIGHT / 2) + (HEIGHT * level));
+
+            graphics.DrawLine(Pens.Black, oldCenter, center);
+
+            //сдвиг в центр круга
+            graphics.FillEllipse(new SolidBrush(COLOR_NODE), center.X - RADIUS, center.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+            graphics.DrawString(data.ToString(), this.Font, new SolidBrush(COLOR_TEXT), new Point(center.X - 10, center.Y - 8));
+            //
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //DrawNode(e.Graphics, this.Width, 0, 15, 0);
+            //DrawNode(e.Graphics, this.Width, 0, 100, 1);
+            Draw(e.Graphics, this.Width - 2 * RADIUS, RADIUS, tree);
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            //перерисовать принудительно
+            Invalidate();
+        }
+    }
+    public class Tree
+    {
+        public int Data;
+        public Tree Left;
+        public Tree Right;
+
+        public Tree(int data, Tree left = null, Tree right = null)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public static string Output(Tree tree)
+        {
+            string s = "";
+            if (tree.Left != null)
+                s += Output(tree.Left) + ", ";
+            if (tree.Right != null)
+                s += Output(tree.Right) + ", ";
+            s += tree.Data.ToString();
+            return s;
+        }
+
+        public static Tree MakeTree(int[] array)
+        {
+            int number = 0;
+            return MakeTree(array, array.Length, ref number);
+        }
+
+        public static Tree MakeTree(int[] array, int count, ref int number)
+        {
+            var tree = new Tree(array[number]);
+            number++;
+            int nl = count / 2;
+            if (nl > 0)
+                tree.Left = MakeTree(array, nl, ref number);
+            int nr = count - nl - 1;
+            if (nr > 0)
+                tree.Right = MakeTree(array, nr, ref number);
+            return tree;
+        }
+    }
+}
